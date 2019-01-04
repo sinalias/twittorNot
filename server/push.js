f9dcaed45a93837f5a9ad30112970d853c682f13
Roberto Perez <sinalias@gmail.com>
2019-01-04 19:34:41 +0100

    incorpora codigo eliminar suscripciones

--- server/push.js
+++ server/push.js
@@ -4,7 +4,7 @@ const urlsafeBase64 =require('urlsafe-base64');
 const vapid=require('./vapid.json');
 const webpush=require('web-push');
 
-const suscripciones = require('./subs-db.json');
+let suscripciones = require('./subs-db.json');
 
 webpush.setVapidDetails("mailto:sinalias@gmail.com",
                     vapid.publicKey,
@@ -22,7 +22,27 @@ module.exports.addSubscription = (suscripcion) => {
 };
 
 module.exports.sendPush = ( post) =>{
+
+    const notificacionesEnviadas = [];
+
     suscripciones.forEach( (suscripcion,i) => {
-        webpush.sendNotification(suscripcion, JSON.stringify(post));
+        
+        const pushProm = webpush.sendNotification(suscripcion, JSON.stringify(post))
+            .then(console.log('Notificacion enviada'))
+            .catch(err => {
+                console.log('La notificación fallo');
+
+                if(err.statusCode === 410){ // ya no existe
+                    suscripciones[i].borrar = true;
+                }
+            });
+
+        notificacionesEnviadas.push(pushProm);
+    });
+
+    Promise.all(notificacionesEnviadas).then(() => {
+        suscripciones = suscripciones.filter(subs => !subs.borrar);
+
+        fs.writeFileSync(`${ __dirname }/subs-db.json`, JSON.stringify(suscripciones) );
     });
 };
